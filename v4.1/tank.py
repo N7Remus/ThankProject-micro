60a743a3292ebfe02493345a05c77886d6c717d9
remus <remaigabor@gmail.com>
2020-01-18 19:21:48 +0100

    tisztítás

--- v4.1/tank.py
+++ v4.1/tank.py
@@ -309,7 +309,7 @@ def index():
     # return render_template("index.html")
     # return render_template("joystick.html")
     # visszaadja a render sablont           //new
-    return render_template("basic.html")
+    return render_template("basic3.html")
     #basic2 -kamerateszt
 
 def detect_motion():
